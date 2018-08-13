95bed1353fa02514c15b23b60b3151365965bbfa
agin <agin@softwareassociates.in>
2018-08-13 14:21:52 +0530

    set operation and filter operation

--- Calculator/Calculator.php
+++ Calculator/Calculator.php
@@ -5,11 +5,23 @@ namespace Calculator;
 
 class Calculator
 {
-    public function setOperations() {
-
+    protected $operations = [];
+    public function setOperation(OperationsInterface $operation) {
+        $this->operations[] = $operation;
     }
 
+    public function setOperations(array $operations) {
+//        foreach ($operations as $index => $operation) {
+//            if (!$operation instanceof OperationsInterface) {
+//                unset($operations[$index]);
+//            }
+//        }
+        $filteredOps = array_filter($operations, function ($operation) {
+            return $operation instanceof OperationsInterface ;
+        });
+        $this->operations = array_merge($this->operations, $filteredOps);
+    }
     public function getOperations() {
-
+        return $this->operations;
     }
 }
--- tests/unit/calculator/CalculatorTest.php
+++ tests/unit/calculator/CalculatorTest.php
@@ -10,7 +10,32 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
         $addition->setOperands([5,3]);
 
         $calculator = new Calculator();
-        $calculator->setOperations($addition);
+        $calculator->setOperation($addition);
+
+        //var_dump($calculator->getOperations());
+        $this->assertCount(1,$calculator->getOperations());
+    }
+
+    /** @test */
+    public function can_set_multiple_operations() {
+        $addition_1 = new Addition;
+        $addition_1->setOperands([5,3]);
+
+        $addition_2 = new Addition;
+        $addition_2->setOperands([2,3]);
+
+        $calculator = new Calculator();
+        $calculator->setOperations([$addition_1, $addition_2]);
+        $this->assertCount(2,$calculator->getOperations());
+    }
+
+    /** @test */
+    public function verify_operations_instance() {
+        $addition = new Addition;
+        $addition->setOperands([5,3]);
+
+        $calculator = new Calculator();
+        $calculator->setOperations(['sec','hai',$addition,'string1','string1']);
 
         $this->assertCount(1,$calculator->getOperations());
     }
